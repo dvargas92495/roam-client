4da136cbd3c90bf44556acd4ab7d98d6197b8c1f
David Vargas <dvargas92495@gmail.com>
2020-10-19 19:22:14 -0400

    fix test

--- tests/dom.test.ts
+++ tests/dom.test.ts
@@ -3,7 +3,6 @@ import { createIconButton } from "../src";
 test("Icon button is rendered", () => {
   const iconButton = createIconButton("sort");
   expect(iconButton).toMatchInlineSnapshot(
-    "",
     `
     <span
       class="bp3-button bp3-minimal bp3-small"
