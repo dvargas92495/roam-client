d68d5787a6af4ab0cf8779422d53a88c80b5fe14
David Vargas <dvargas92495@gmail.com>
2020-11-18 12:25:48 -0500

    Add block order type and action

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ declare global {
       ) => {
         ":block/children": { ":db/id": number }[];
         ":block/string": string;
+        ":block/order": string;
       };
     };
     roamDatomicAlphaAPI?: (
