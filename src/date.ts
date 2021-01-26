f10d94d97ec2503ee0895250987e6b5dc34a5859
David Vargas <dvargas92495@gmail.com>
2021-01-26 16:39:24 -0500

    Invalid date handling

--- src/date.ts
+++ src/date.ts
@@ -4,6 +4,8 @@ import parse from "date-fns/parse";
 export const parseRoamDate = (s: string) =>
   parse(s, "MMMM do, yyyy", new Date());
 
-export const toRoamDate = (d: Date) => format(d, "MMMM do, yyyy");
+export const toRoamDate = (d: Date) =>
+  isNaN(d.valueOf()) ? "" : format(d, "MMMM do, yyyy");
 
-export const toRoamDateUid = (d: Date) => format(d, "MM-dd-yyyy");
+export const toRoamDateUid = (d: Date) =>
+  isNaN(d.valueOf()) ? "" : format(d, "MM-dd-yyyy");
