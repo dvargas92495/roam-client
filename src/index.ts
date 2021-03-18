5134da92f920b9e1da187a6859eabd9d38397a4f
David Vargas <dvargas92495@gmail.com>
2021-03-18 17:00:03 -0500

    getpageviewtype

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,7 @@ export {
   getPageTitleByBlockUid,
   getPageTitleByPageUid,
   getPageTitleReferencesByPageTitle,
+  getPageViewType,
   getParentTextByBlockUid,
   getParentTextByBlockUidAndTag,
   getParentUidByBlockUid,
--- src/queries.ts
+++ src/queries.ts
@@ -216,3 +216,8 @@ export const getAllPageNames = (): string[] =>
   window.roamAlphaAPI
     .q("[:find ?s :where [?e :node/title ?s]]")
     .map((b) => b[0] as string);
+
+export const getPageViewType = (title: string): ViewType => 
+  window.roamAlphaAPI.q(
+    `[:find ?v :where [?e :children/view-type ?v] [?e :node/title "${normalizePageTitle(title)}"]]`
+  )?.[0]?.[0] as ViewType
