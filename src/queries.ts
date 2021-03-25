7ea6f831db958b58fbcbba33c1b273ae4cd244e7
David Vargas <dvargas92495@gmail.com>
2021-03-25 14:34:01 -0500

    Missing uid quotes

--- src/queries.ts
+++ src/queries.ts
@@ -254,6 +254,6 @@ export const getBlockUidsReferencingPage = (title: string): string[] =>
 export const getPageTitlesReferencingBlockUid = (uid: string): string[] =>
   window.roamAlphaAPI
     .q(
-      `[:find ?t :where [?r :block/uid ${uid}] [?b :block/refs ?r] [?b :block/page ?p] [?p :node/title ?t]]`
+      `[:find ?t :where [?r :block/uid "${uid}"] [?b :block/refs ?r] [?b :block/page ?p] [?p :node/title ?t]]`
     )
     .map((s) => s[0]);
