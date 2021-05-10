23f05d6a6be00d563ff5da3615e3abbd5e15530c
David Vargas <dvargas92495@gmail.com>
2021-05-10 13:41:37 -0600

    1.60.2

--- src/dom.ts
+++ src/dom.ts
@@ -417,7 +417,7 @@ const getDomRefs = (blockUid: string) => {
       currentlyEditingBlock.value.match(/\(\(([\w\d-]{9})\)\)/g) || []
     ).map((s) => s.slice(2, -2));
   }
-  return []
+  return [];
 };
 
 export const getReferenceBlockUid = (
