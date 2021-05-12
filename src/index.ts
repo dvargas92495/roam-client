f485f760ff429b623905673bd79a26d27492a288
David Vargas <dvargas92495@gmail.com>
2021-05-11 21:42:47 -0600

    extractTag

--- src/index.ts
+++ src/index.ts
@@ -275,3 +275,14 @@ export const getCurrentUserEmail = () => {
   }
   return "";
 };
+
+export const extractTag = (tag: string): string =>
+  tag.startsWith("#[[") && tag.endsWith("]]")
+    ? tag.substring(3, tag.length - 2)
+    : tag.startsWith("[[") && tag.endsWith("]]")
+    ? tag.substring(2, tag.length - 2)
+    : tag.startsWith("#")
+    ? tag.substring(1)
+    : tag.endsWith("::")
+    ? tag.substring(0, tag.length - 2)
+    : tag;
